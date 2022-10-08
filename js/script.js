053a572e3906c3b895dddd0571d5c54aaebf3a61
Isaac Kwemboi Tegei <isaac.kwemboi@student.moringaschool.com>
2022-10-08 12:24:08 +0300

    get day

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 var daysOfWeek = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']; // WE DECLARE AND INITIALISE AN ARRAY WITH THE DAYS OF THE WEEK STARTING FROM SUNDAY TO MONDAY
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //ARRAY OF FEMALE AKAN NAMES
 var maleNames = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]; //ARRAY OF MALE AKAN NAMES
@@ -10,5 +9,9 @@ console.log(dateInput)
 var birthDate = new Date(dateInput) //uses the variable dateinput from the user prompt to create a calender date
 console.log(birthDate)
 
-var tareheKamili = birthDate.getDate();
-console.log(tareheKamili);
+var sikuKamili = birthDate.getDay();
+console.log(sikuKamili);
+
+var genderFemale = femaleNames[sikuKamili];
+var genderMale = maleNames[sikuKamili];
+console.log(genderMale, genderFemale);
