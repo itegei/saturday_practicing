9bcba25edb4aad94a4f939e6f34accf88f724fd2
Isaac Kwemboi Tegei <isaac.kwemboi@student.moringaschool.com>
2022-10-08 15:50:54 +0300

    date validationt

--- js/script.js
+++ js/script.js
@@ -1,33 +1,62 @@
-var daysOfWeek = ['Sunday', 'Monday', 'Tuesday','Wednesday', 'Thursday', 'Friday', 'Saturday']; // WE DECLARE AND INITIALISE AN ARRAY WITH THE DAYS OF THE WEEK STARTING FROM SUNDAY TO MONDAY
+var daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+]; // WE DECLARE AND INITIALISE AN ARRAY WITH THE DAYS OF THE WEEK STARTING FROM SUNDAY TO MONDAY
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //ARRAY OF FEMALE AKAN NAMES
-var maleNames = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]; //ARRAY OF MALE AKAN NAMES
-
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; //ARRAY OF MALE AKAN NAMES
 
 /*Date MANIPULATION VARIABLES*/
 var dateInput = prompt("Enter date of birth: (YYYY-MM-DD)");
-var birthDate = new Date(dateInput) //uses the variable dateinput from the user prompt to create a calender date
+var birthDate = new Date(dateInput); //uses the variable dateinput from the user prompt to create a calender date
 var sikuKamili = birthDate.getDay();
-var month = birthDate.getMonth()
-var month = birthDate.getDate()
+var birthMonth = birthDate.getMonth();
+var birthYear = birthDate.getFullYear();
 
+console.log(sikuKamili, birthMonth, birthYear);
 
 /*USING THE DATES TO ACCESS THE NAME LISTS*/
 var genderFemale = femaleNames[sikuKamili];
 var genderMale = maleNames[sikuKamili];
 
-var jinsia = prompt("Enter the gender: (M/F)").toLowerCase();  //takes the user input and converts it to upper case
-if (jinsia === "m"){ alert("Your Akan name is " +genderMale);}
-else{alert("Your Akan name is " +genderFemale);}
+var jinsia = prompt("Enter the gender: (M/F)").toLowerCase(); //takes the user input and converts it to upper case
 
-if (jinsia === "M" || jinsia === "F" || jinsia === "MALE" || jinsia === "FEMALE") {
-    alert ("Twende kazi")
-}
-else{
-    alert("Sema kimeumana")
+if (jinsia === "m") {
+  alert("Your Akan name is " + genderMale);
+} else {
+  alert("Your Akan name is " + genderFemale);
 }
 
-// var x = new Date("2022-13-98")
-// console.log(x)
+if (
+  jinsia === "m" ||
+  jinsia === "m" ||
+  jinsia === "MALE" ||
+  jinsia === "FEMALE"
+) {
+  alert("Twende kazi");
+} else {
+  alert("Sema kimeumana");
+}
+
+var x = new Date("");
+console.log(x);
 // typeof(x)
+
+if (
+  sikuKamili >= 0 &&
+  sikuKamili <= 6 &&
+  birthMonth >= 0 &&
+  birthMonth <= 11 &&
+  birthYear >= 1900 &&
+  birthYear <= 2100
+) {
+  alert("Correct date .. onto next");
+} else {
+  alert("Wrong date format, please check");
+}
 
 //console.log(jinsia);
