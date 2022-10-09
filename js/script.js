766e384bf297a953828c57540e066fb15b9dce22
Isaac Kwemboi Tegei <isaac.kwemboi@student.moringaschool.com>
2022-10-09 12:13:43 +0300

    Feedback display

--- js/script.js
+++ js/script.js
@@ -26,11 +26,11 @@ if (sikuKamili >= 0 && sikuKamili <= 7 && birthMonth >= 0 && birthMonth <= 12 &&
     if (jinsia === "m" || jinsia === "f" || jinsia === "MALE" || jinsia === "FEMALE") 
     {
         if (jinsia ==="m"){
-            document.getElementById("myFeedback").innerHTML = "Your Akan Name is " +genderMale
+            document.getElementById("myFeedback").innerHTML = "Your Akan Name is " +genderMale;
             // alert("Your Akan Name is " +genderMale); 
         }
         else {
-            document.getElementById("myFeedback").innerHTML = "Your Akan Name is " +genderFemale
+            document.getElementById("myFeedback").innerHTML = "Your Akan Name is " +genderFemale;
             // alert("Your Akan Name is " +genderFemale); 
         }  
     } 
